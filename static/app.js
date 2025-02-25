810b2b8d9808f7c537f1d0362b89b5a74e9c869a
atrox3d <atrox3d@gmail.com>
2025-02-25 18:14:51 +0100

    refactor js

---
+++ static/app.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const prompt = form.elements.prompt.value
+        form.elements.prompt.value = ''
+
+        fetch('/palette', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                prompt: prompt
+            })
+        })
+        .then((response) => response.json())
+        .then(data => {
+            const colors = data.colors;
+            const container = document.querySelector('.container');
+            container.innerHTML = '';
+            for(const color of colors) {
+                const div = document.createElement('div')
+                div.classList.add('color')
+                div.style.backgroundColor = color;
+                div.style.width = `calc(100%/ ${colors.length}`;
+                div.addEventListener('click', function(){
+                    navigator.clipboard.writeText(color);
+                });
+                const span = document.createElement('span');
+                span.innerText = color;
+                div.appendChild(span);
+                container.appendChild(div);
+            }
+        });
+    });
+});

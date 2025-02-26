3ddc2e23f8d516ffe58cba54454acbbaf02619ce
atrox3d <atrox3d@gmail.com>
2025-02-26 11:56:58 +0100

    finding a way to add commands

--- chatbot.py
+++ chatbot.py
@@ -88,11 +88,24 @@ def ask(*messages:str, client:openai.OpenAI=get_client(), model=MODEL, **kwargs)
     return reply
 
 
-COMMANDS = '/history'.split()
-def command(prompt:str, commands=COMMANDS):
-    if any(prompt.startswith(cmd) for cmd in commands):
-        return True
+#
+#   TODO: find a way to run commands with params or globals
+#
+COMMANDS = {
+    '/history': {'fn': None, 'args':['history']}
+}
+def command(prompt:str, commands:dict=COMMANDS, **kwargs):
+    # if prompt.startswith('/') and any(prompt.startswith(cmd) for cmd in commands.keys()):
+    for cmd in commands.keys():
+        if prompt.startswith(cmd):
+            # TODO: process command
+            cmd, *rest = cmd.split()
+            print(f'command!', cmd, rest)
+            return True
     return False
+#
+#   TODO: find a way to run commands with params or globals
+#
 
 
 def setup_history(
@@ -131,11 +144,10 @@ def main(
         
         while True:
             prompt = user_input(user_prompt)
-            # if command(prompt):
-                # continue
+            if command(prompt):
+                continue
             
             if proceed(prompt):
-                
                 history.append(user_message(prompt))
                 reply = ask(*history, model=model)
                 history.append(assistant_message(get_message_content(reply)))

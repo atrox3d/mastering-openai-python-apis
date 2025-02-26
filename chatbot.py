4b97aebac6e97dd0ce30fb74bf41dbe3791fa61f
atrox3d <atrox3d@gmail.com>
2025-02-26 06:54:34 +0100

    setup minimal loop

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 
@@ -5,13 +6,18 @@ import openai
 from dotenv import load_dotenv
 
 
+MODEL = 'gpt-4o-mini'
+DOTENV = '.env'
+APIKEY_ENV_VAR = 'OPENAI_API_KEY'
+CLIENT = None
 
-def check_openai_key() -> bool:
+
+def check_openai_key(dotenv:str=DOTENV, apikey_env_var:str=APIKEY_ENV_VAR) -> bool:
     load_dotenv()
-    api_key = os.getenv('OPENAI_API_KEY')
+    api_key = os.getenv(APIKEY_ENV_VAR)
     if api_key is None:
-        print('missing env variable OPENAI_API_KEY')
-        if not Path('.env').exists():
+        print(f'missing env variable {APIKEY_ENV_VAR}')
+        if not Path(dotenv).exists():
             print('and no .env file found')
         print('exiting')
         return False
@@ -19,6 +25,41 @@ def check_openai_key() -> bool:
     return True
 
 
-if __name__ == "__main__":
+def get_client():
+    global CLIENT
+    CLIENT = CLIENT or openai.OpenAI()
+    return CLIENT
+
+
+def message(role:str, content:str) -> dict:
+    return {'role': role, 'content': content}
+
+
+def ask(prompt:str, client:openai.OpenAI=get_client(), model=MODEL, **kwargs):
+    reply = client.chat.completions.create(
+        messages=[message('user', prompt)],
+        model=model
+    )
+    return reply
+
+
+def main():
     if not check_openai_key():
         exit(1)
+    try:
+        while True:
+            prompt = input('You: ')
+            if prompt.lower() == 'bye':
+                break
+            reply = ask(prompt)
+            # print(json.dumps(reply, indent=4))
+            print(reply.choices[0].message.content)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('exiting...')
+
+
+
+if __name__ == "__main__":
+    main()
--- select-vscode-venv.py
+++
@@ -1,3 +0,0 @@
-'''
-this file serves only to allow .venv selection to activate the local environment for the terminal in vscode
-'''

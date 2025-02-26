235b84b12348f1d2b26b8556636da5cc0f65117d
atrox3d <atrox3d@gmail.com>
2025-02-26 07:35:44 +0100

    setup chat history

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ MODEL = 'gpt-4o-mini'
 DOTENV = '.env'
 APIKEY_ENV_VAR = 'OPENAI_API_KEY'
 CLIENT = None
-STOP_COMMANDS = 'bye stop end quit abort'.split()
+STOP_COMMANDS = 'bye stop end quit abort exit'.split()
 
 
 def check_openai_key(dotenv:str=DOTENV, apikey_env_var:str=APIKEY_ENV_VAR) -> bool:
@@ -56,16 +56,6 @@ def assistant_message(content:str) -> dict:
     return message('assistant', content)
 
 
-def ask(prompt:str, client:openai.OpenAI=get_client(), model=MODEL, **kwargs) -> ChatCompletion:
-    '''query openai llm and returns full reply'''
-    reply = client.chat.completions.create(
-        messages=[user_message(prompt)],
-        model=model,
-        **kwargs
-    )
-    return reply
-
-
 def user_input(prompt:str='You: ') -> str:
     '''get prompt from user'''
     return input(prompt)
@@ -76,10 +66,26 @@ def proceed(prompt:str, stop_commands:list[str]=STOP_COMMANDS) -> bool:
     return prompt.lower() not in stop_commands
 
 
+def get_message_content(completion:ChatCompletion) -> str:
+    content = completion.choices[0].message.content
+    return content
+
+
 def process_answer(reply:ChatCompletion):
     '''do whatever is needed with the answer'''
-    answer = reply.choices[0].message.content
+    answer = get_message_content(reply)
     print(f'Bot: {answer}')
+
+
+def ask(*messages:str, client:openai.OpenAI=get_client(), model=MODEL, **kwargs) -> ChatCompletion:
+    '''query openai llm and returns full reply'''
+    print(f'{messages = }')
+    reply = client.chat.completions.create(
+        messages=messages,
+        model=model,
+        **kwargs
+    )
+    return reply
 
 
 def main():
@@ -87,10 +93,15 @@ def main():
     if not check_openai_key():
         exit(1)
     try:
+        history = []
         while True:
             prompt = user_input()
             if proceed(prompt):
-                reply = ask(prompt)
+                
+                history.append(user_message(prompt))
+                reply = ask(*history)
+                history.append(assistant_message(get_message_content(reply)))
+                
                 process_answer(reply)
             else:
                 break
